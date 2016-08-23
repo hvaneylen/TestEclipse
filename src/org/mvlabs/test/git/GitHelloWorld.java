71139537bd1a685ac1693ca8e553c547a9be9141
hvaneylen <hvaneylen@sodifrance.fr>
2016-08-23 14:20:24 +0200

    initial commit

---
+++ src/org/mvlabs/test/git/GitHelloWorld.java
@@ -0,0 +1,10 @@
+package org.mvlabs.test.git;
+
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world from git !");
+	}
+
+}
